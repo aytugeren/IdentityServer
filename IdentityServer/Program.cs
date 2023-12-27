e5161bce99548dc0e74b3bd089cf089bdfbda733
Aytuğ Eren <aytgeren@gmail.com>
2023-12-27 08:10:16 +0300

    Add project files.

---
+++ IdentityServer/Program.cs
@@ -0,0 +1,17 @@
+using IdentityServer;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(Config.Clients)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddDeveloperSigningCredential();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseIdentityServer();
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
---
+++ Movies.API/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MoviesAPIContext>(options =>
+    options.UseInMemoryDatabase("Movies"));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+SeedDatabase(app);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+static void SeedDatabase(WebApplication builder)
+{
+    using (var scope = builder.Services.CreateScope())
+    {
+        var service = scope.ServiceProvider;
+        var moviesContext = service.GetRequiredService<MoviesAPIContext>();
+        MoviesContextSeed.SeedAsync(moviesContext);
+    }
+}
